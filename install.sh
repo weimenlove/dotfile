bfe09ba9ff83e06293bdad164426a26a1cc4964d
Matthew Wei <weimenlove@gmail.com>
2015-08-24 22:54:19 +0800

    add git config and emacspace config

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ rm -f ~/.bashrc
 ln -s ~/.dotfile/dotbashrc/bashrc ~/.bashrc
 rm -f ~/.vimrc
 ln -s ~/.dotfile/dotvim/vimrc ~/.vimrc
-
+rm -f ~/.gitconfig
 ln -s ~/.dotfile/dotgitconfig/gitconfig ~/.gitconfig
+rm -f ~/.gitignore
 ln -s ~/.dotfile/dotgitconfig/gitignore ~/.gitignore
+rm -f ~/.spacemacs
+ln -s ~/.dotfile/dotspacemacs ~/.spacemacs
 
 git submodule add git@github.com:tpope/vim-pathogen.git dotvim/bundle/vim-pathogen
 git submodule add git@github.com:msanders/snipmate.vim.git dotvim/bundle/vim-snipmate
