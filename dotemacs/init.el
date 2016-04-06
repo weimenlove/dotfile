2cad90df3cece2d5400b2ae6970228b3f613a585
Matthew Wei <weimenlove@gmail.com>
2016-04-06 23:33:36 +0800

    [emacs] add org-mode for emacs

--- dotemacs/init.el
+++ dotemacs/init.el
@@ -1,11 +1,44 @@
+;; -*- coding: utf-8 -*-
+
 ;;Personal information
 (setq user-full-name "Fantity Wei"
-	  user-mail-address "weimenlove@gmail.com")
+  user-mail-address "weimenlove@gmail.com")
 
 ;;Emacs Initialization
 (setq emacs-load-start-time (current-time))
 (add-to-list 'load-path "~/.emacs.d/lisp")
 ;;(add-to-list 'load-path "~/elisp")
+
+;;----------------------------------------------------------------------------
+;; Which functionality to enable (use t or nil for true and false)
+;;----------------------------------------------------------------------------
+(setq *macbook-pro-support-enabled* t)
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *is-carbon-emacs* (and *is-a-mac* (eq window-system 'mac)))
+(setq *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
+(setq *win32* (eq system-type 'windows-nt) )
+(setq *cygwin* (eq system-type 'cygwin) )
+(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
+(setq *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)) )
+(setq *linux-x* (and window-system *linux*) )
+(setq *xemacs* (featurep 'xemacs) )
+(setq *emacs24* (and (not *xemacs*) (or (>= emacs-major-version 24))) )
+(setq *no-memory* (cond
+  (*is-a-mac*
+   (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
+  (*linux* nil)
+  (t nil)))
+
+;;----------------------------------------------------------------------------
+;; Less GC, more memory
+;;----------------------------------------------------------------------------
+(defun my-optimize-gc (NUM PER)
+  "By default Emacs will initiate GC every 0.76 MB allocated (gc-cons-threshold == 800000).
+  @see http://www.gnu.org/software/emacs/manual/html_node/elisp/Garbage-Collection.html
+  We increase this to 16MB by `(my-optimize-gc 16 0.5)` "
+  (setq-default gc-cons-threshold (* 1024 1024 NUM)
+    gc-cons-percentage PER))
+
 
 ;;Backups
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
@@ -17,7 +50,7 @@
 (when window-system
   (tooltip-mode -1)
   (tool-bar-mode -1)
-  (menu-bar-mode -1))
+  (menu-bar-mode t))
 
 ;;Sentences end with a single space
 (setq sentence-end-double-space nil)
@@ -58,3 +91,17 @@
 (require 'init-color-theme)
 (require 'init-dired)
 (require 'init-linum-mode)
+(require 'init-org)
+(require 'init-org-mime)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(org-agenda-files (quote ("~/work/work.org"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )
