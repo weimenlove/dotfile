9d0392a1061d271a51274b084bd0425b53bf0057
Matthew Wei <weimenlove@gmail.com>
2016-07-13 23:12:45 +0800

    add windown-number configure as redguardtoo

--- dotemacs/init.el
+++ dotemacs/init.el
@@ -100,12 +100,13 @@
 (require 'init-linum-mode)
 (require 'init-org)
 (require 'init-org-mime)
+(require 'init-window-numbering)
 (custom-set-variables
 ;; custom-set-variables was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
 ;; Your init file should contain only one such instance.
 ;; If there is more than one, they won't work right.
-'(org-agenda-files (quote ("~/Dropbox/org/work.org"))))
+'(org-agenda-files (quote ("~/Dropbox/org/goal.org" "~/Dropbox/org/work.org"))))
 (custom-set-faces
 ;; custom-set-faces was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ dotemacs/lisp/init-window-numbering.el
@@ -0,0 +1,11 @@
+;; https://github.com/abo-abo/ace-window
+;; `M-x ace-window ENTER m` to swap window
+(global-set-key (kbd "C-x o") 'ace-window)
+
+;; {{ move focus between sub-windows
+(require 'window-numbering)
+(custom-set-faces '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold)))))
+(window-numbering-mode 1)
+;; }}
+
+(provide 'init-window-numbering)
