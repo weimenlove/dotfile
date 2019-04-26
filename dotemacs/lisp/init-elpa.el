f8ce6dc655b40d6ed395a1246d3c1a8d95d5507d
Matthew Wei <weimenlove@gmail.com>
2019-04-26 20:57:02 +0800

    change emacs package soure

--- dotemacs/lisp/init-elpa.el
+++ dotemacs/lisp/init-elpa.el
@@ -1,14 +1,12 @@
 
 ;; http://melpa.org/#/getting-started
 (require 'package) ;; You might already have this line
-;;(add-to-list 'package-archives
-;;             '("melpa" . "https://melpa.org/packages/"))
-  ;;这个是国内一个elpa仓库镜像，速度更快一些，你懂得
-  (add-to-list 'package-archives '("popkit" . "http://elpa.popkit.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                           ("melpa" . "http://elpa.emacs-china.org/melpa/")
+                           ("Marmalade" . "http://elpa.emacs-china.org/marmalade/")
+                           ("org" . "http://elpa.emacs-china.org/org/")
+                           ))
+(package-initialize) ;; You might already have this line'')')
 
 ;;; ------------------------------------------------------------------
 ;;; ------------------------------------------------------------------
